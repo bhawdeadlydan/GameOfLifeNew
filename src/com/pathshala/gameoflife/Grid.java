cfe4d29a18c8552a10624cc682ac5ec519bfcaa4
Bhawesh Kumar Singh <bhawdeadlydan@gmail.com>
2015-06-08 12:06:22 +0530

    Bhawesh - Added Spec for checking if added living cell is dead after killing

--- src/com/pathshala/gameoflife/Grid.java
+++ src/com/pathshala/gameoflife/Grid.java
@@ -1,8 +1,32 @@
 package com.pathshala.gameoflife;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Grid {
+    private HashMap<ArrayList<Integer>, Boolean> gridOfLife = new HashMap<ArrayList<Integer>, Boolean>();
 
 
+    public void addLivingCell(int xCoordinate, int yCoordinate) {
+        ArrayList<Integer> coordinates = new ArrayList<Integer>();
+        coordinates.add(0, xCoordinate);
+        coordinates.add(1, yCoordinate);
+          gridOfLife.put(coordinates, true);
+    }
 
+    public void killCell(int xCoordinate, int yCoordinate) {
+        ArrayList<Integer> coordinates = new ArrayList<Integer>();
+        coordinates.add(0, xCoordinate);
+        coordinates.add(1, yCoordinate);
+        gridOfLife.put(coordinates, true);
+
+    }
+
+    public boolean isAlive(int xCoordinate, int yCoordinate) {
+        ArrayList<Integer> coordinates = new ArrayList<Integer>();
+        coordinates.add(0, xCoordinate);
+        coordinates.add(1, yCoordinate);
+        return gridOfLife.get(coordinates);
+    }
 }
--- test/com/pathshala/gameoflife/GridTest.java
+++ test/com/pathshala/gameoflife/GridTest.java
@@ -3,11 +3,28 @@ package com.pathshala.gameoflife;
 import junit.framework.TestCase;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 
 public class GridTest {
 
+    @Test
+    public void ShouldBeAbleToMakeAlivingCellDead() {
+        Grid infiniteGrid = new Grid();
+        infiniteGrid.addLivingCell(4, 5);
+
+        infiniteGrid.killCell(4, 5);
+
+        boolean expected = true;
+        boolean actual = infiniteGrid.isAlive(4, 5);
+
+        assertThat(expected, is(actual));
+    }
+
     
-
-
 
 }
